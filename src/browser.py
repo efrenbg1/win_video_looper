912ce73feab7ca0d14c6953309dfbda98fbc0f11
efrenbg1 <efren@boyarizo.es>
2021-06-20 15:04:17 +0200

    Added support for raspberry pi browser

--- src/browser.py
+++ src/browser.py
@@ -1,10 +1,18 @@
 import os
+import sys
+import subprocess
 
 
 def stop():
-    os.system('taskkill /f /im msedge.exe >nul 2>&1')
+    if sys.platform == 'win32':
+        os.system('taskkill /f /im msedge.exe >nul 2>&1')
+    else:
+        os.system('pkill -9 chromium')
 
 
 def start():
     stop()
-    os.system('start /b "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --kiosk http://localhost:5000/projector --edge-kiosk-type=fullscreen')
+    if sys.platform == 'win32':
+        os.system('start /b "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --kiosk http://localhost:5000/projector --edge-kiosk-type=fullscreen')
+    else:
+        subprocess.Popen(['chromium-browser', '--start-fullscreen', '--kiosk', 'http://localhost:5000/projector'])
